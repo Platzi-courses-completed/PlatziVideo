34dd10be62ce6fb9e6a66d94fa1df000ef16dd98
Jesús Mejía <51868559+JMR-Mejia@users.noreply.github.com>
2021-05-17 18:57:08 -0500

    Integración de-Express-con-React-2 (#2)
    * Servir React con Express
    
    * React Router, history y initialState accesible

--- src/server/server.js
+++ src/server/server.js
@@ -1,17 +1,18 @@
-import express from "express";
-import dotenv from "dotenv";
-import webpack from "webpack";
+/* eslint-disable global-require */
+import express from 'express';
+import dotenv from 'dotenv';
+import webpack from 'webpack';
 
 dotenv.config();
 
 const { ENV, PORT } = process.env;
 const app = express();
 
-if (ENV === "development") {
-  console.log("development config");
-  const webpackConfig = require("../../webpack.config");
-  const webpackDevMiddleware = require("webpack-dev-middleware");
-  const webpackHotMiddleware = require("webpack-hot-middleware");
+if (ENV === 'development') {
+  console.log('development config');
+  const webpackConfig = require('../../webpack.config');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
   const compiler = webpack(webpackConfig);
   const serverConfig = { port: PORT, hot: true };
 
@@ -19,14 +20,23 @@ if (ENV === "development") {
   app.use(webpackHotMiddleware(compiler));
 }
 
-app.get("*", (req, res) => {
-  console.log("Hola");
-  res.send({
-    hello: "express",
-  });
+app.get('*', (req, res) => {
+  res.send(`
+  <!DOCTYPE html>
+    <html>
+    <head>
+        <link rel="stylesheet" href="assets/app.css" type="text/css">
+        <title>Platzi Video</title>
+    </head>
+    <body>
+        <div id="app"></div>
+        <script src="assets/app.js" type="text/javascript"></script>
+    </body>
+    </html>
+  `);
 });
 
 app.listen(PORT, (err) => {
   if (err) console.log(err);
-  else console.log(`Server running on port 3000`);
+  else console.log('Server running on port 3000');
 });
