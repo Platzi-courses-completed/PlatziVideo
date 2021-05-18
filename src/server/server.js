8a665b4791329879db809d6a329fc282aa52b905
JMR-Mejia <chucho4720013@gmail.com>
2021-05-17 23:23:51 -0500

    Configurando nuestro servidor para producción

--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import webpack from 'webpack';
+import helmet from 'helmet';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { Provider } from 'react-redux';
@@ -28,6 +29,11 @@ if (ENV === 'development') {
 
   app.use(webpackDevMiddleware(compiler, serverConfig));
   app.use(webpackHotMiddleware(compiler));
+} else {
+  app.use(express.static(`${__dirname}/public`));
+  app.use(helmet());
+  app.use(helmet.permittedCrossDomainPolicies());
+  app.disable('x-powered-by');
 }
 
 const setResponse = (html, preloadedState) => {
@@ -63,6 +69,7 @@ const renderApp = (req, res) => {
     </Provider>,
   );
 
+  res.removeHeader('x-powered-by');
   res.send(setResponse(html, preloadedState));
 };
 
