409701e2570b0fb117876df4e3b555b086b32e37
JMR-Mejia <chucho4720013@gmail.com>
2021-05-17 19:38:46 -0500

    función principal para el renderizado del servidor

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,7 @@
-require("@babel/register")({
-  presets: ["@babel/preset-env", "@babel/preset-react"],
+require('ignore-styles');
+
+require('@babel/register')({
+  presets: ['@babel/preset-env', '@babel/preset-react'],
 });
 
-require("./server");
+require('./server');
