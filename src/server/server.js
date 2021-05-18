f511692086e00ddee18b2ad5c1d6f28bc9204855
JMR-Mejia <chucho4720013@gmail.com>
2021-05-18 14:26:48 -0500

    Aplicar hashes al nombre de nuestros builds

--- src/server/server.js
+++ src/server/server.js
@@ -13,6 +13,7 @@ import { StaticRouter } from 'react-router-dom';
 import serverRoutes from '../frontend/routes/serverRoutes';
 import reducer from '../frontend/reducers';
 import initialState from '../frontend/initialState';
+import getManifest from './getManifest';
 
 dotenv.config();
 
@@ -48,12 +49,15 @@ if (ENV === 'development') {
   app.disable('x-powered-by');
 }
 
-const setResponse = (html, preloadedState) => {
+const setResponse = (html, preloadedState, manifest) => {
+  const mainStyles = manifest ? manifest['main.css'] : 'assets/app.css';
+  const mainBuild = manifest ? manifest['main.js'] : 'assets/app.js';
+
   return `
   <!DOCTYPE html>
     <html>
     <head>
-        <link rel="stylesheet" href="assets/app.css" type="text/css">
+        <link rel="stylesheet" href="${mainStyles}" type="text/css">
         <title>Platzi Video</title>
     </head>
     <body>
@@ -64,7 +68,7 @@ const setResponse = (html, preloadedState) => {
     '\\u003c',
   )}
         </script>
-        <script src="assets/app.js" type="text/javascript"></script>
+        <script src="${mainBuild}" type="text/javascript"></script>
     </body>
     </html>
   `;
@@ -82,7 +86,7 @@ const renderApp = (req, res) => {
   );
 
   res.removeHeader('x-powered-by');
-  res.send(setResponse(html, preloadedState));
+  res.send(setResponse(html, preloadedState, getManifest()));
 };
 
 app.get('*', renderApp);
